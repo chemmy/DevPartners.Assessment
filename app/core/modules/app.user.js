ed49d67eeb4586328ef577db56aea3d3df65f56e
DAV-DT13\chemm <chemmy0430@gmail.com>
2018-04-17 19:10:41 -0700

    Add initial user module and routing

---
+++ app/core/modules/app.user.js
@@ -0,0 +1,23 @@
+(function(){
+
+    angular
+        .module('app.user', [
+            'app.core'
+        ])
+        .config(config);
+
+    /* ------------------------------------------------ */
+    
+    function config($stateProvider) {
+        $stateProvider
+            .state('app.user', {
+                abstract: true,
+                url: 'user',
+                templateUrl: 'user/user-index.html',
+            })
+            .state('app.user.dashboard', {
+                url: '',
+                templateUrl: 'user/navigation/user-sidebar.html'
+            });
+    }
+})();
