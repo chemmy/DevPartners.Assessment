6dfc65944626ee92f16e8c8225908c4164373deb
DAV-DT13\chemm <chemmy0430@gmail.com>
2018-04-19 03:08:29 -0700

    Add form constant for api error messages

--- app/core/config/constants.config.js
+++ app/core/config/constants.config.js
@@ -4,7 +4,12 @@
     var app = angular.module('app');
     
     app.constant('API', {
-        'END_POINT': 'http://raphaeldevpartners.pythonanywhere.com'
+        'END_POINT': 'test/data'
+    });
+
+    app.constant('FORM_CONST', {
+        'API_FETCH_FAILED': 'Unable to load data. Please try again later.',
+        'API_ACTION_FAILED': 'Unable to continue action. Please try again later.'
     });
 
 })();
