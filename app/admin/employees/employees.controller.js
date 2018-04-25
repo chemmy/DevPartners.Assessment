361d45ee4944b4de6418330f08fd6c8bc4728f34
DAV-DT13\chemm <chemmy0430@gmail.com>
2018-04-25 02:20:29 -0700

    Create add employee feature

--- app/admin/employees/employees.controller.js
+++ app/admin/employees/employees.controller.js
@@ -126,6 +126,7 @@
         function showEdit(employee) {
             vm.isShowOnly = false;
             vm.employee = employee;
+            vm.selectedPosition = (employee) ? getPosition(employee.position_id) : vm.empPositions[0];
 
             $ngConfirm({
                 title: '',
@@ -137,7 +138,7 @@
                 backgroundDismiss: true,
                 buttons: {
                     btn: {
-                        text: 'Save',
+                        text: (employee) ? 'Update' : 'Save',
                         btnClass: 'btn-warning',
                         action: function(scope, button){
                             vm.employee.position_id = vm.selectedPosition.position_id;
